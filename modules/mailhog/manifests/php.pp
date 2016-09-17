38f1e31b2c87ec582ed76ce5c63473a5bfac7417
Bronson Quick <bronson@sennza.com.au>
2016-09-17 17:31:57 +1000

    Add some logic to handle different config dirs with php

--- modules/mailhog/manifests/php.pp
+++ modules/mailhog/manifests/php.pp
@@ -1,7 +1,16 @@
 class mailhog::php {
-	file { '/etc/php5/conf.d/mailhog.ini':
-		content => template('mailhog/php.ini.erb'),
-		notify => Service['php5-fpm'],
-		require => Package[ 'php5-fpm' ],
+	$vconfig = sz_load_config('/vagrant')
+	if versioncmp( "${$vconfig[php]}", '5.6') > -1 {
+		file { '/etc/php5/fpm/conf.d/mailhog.ini':
+			content => template('mailhog/php.ini.erb'),
+			notify  => Service['php5-fpm'],
+			require => Package['php5-fpm'],
+		}
+	} else {
+		file { '/etc/php/conf.d/mailhog.ini':
+			content => template('mailhog/php.ini.erb'),
+			notify  => Service['php5-fpm'],
+			require => Package['php5-fpm'],
+		}
 	}
 }
