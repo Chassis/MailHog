0839d9973ce79be681003393476e10d1a3642685
Bronson Quick <bronson@sennza.com.au>
2016-11-19 14:32:33 +1000

    Tabs not spaces

--- modules/mailhog/manifests/php.pp
+++ modules/mailhog/manifests/php.pp
@@ -1,18 +1,18 @@
 class mailhog::php (
-  $php = $mailhog_config[php]
+	$php = $mailhog_config[php]
 )
 {
-  if versioncmp( "${$php}", '5.4') <= 0 {
-    $php_package = 'php5'
-    $php_dir = 'php5'
-  }
-  else {
-    $php_package = "$php"
-    $php_dir = "php/$php"
-  }
-  file { "/etc/${php_dir}/fpm/conf.d/mailhog.ini":
-    content => template('mailhog/php.ini.erb'),
-    notify  => Service["php${php_package}-fpm"],
-    require => Package["php${php_package}-fpm"],
-  }
+	if versioncmp( "${$php}", '5.4') <= 0 {
+		$php_package = 'php5'
+		$php_dir = 'php5'
+	}
+	else {
+		$php_package = "$php"
+		$php_dir = "php/$php"
+	}
+	file { "/etc/${php_dir}/fpm/conf.d/mailhog.ini":
+		content => template('mailhog/php.ini.erb'),
+		notify  => Service["php${php_package}-fpm"],
+		require => Package["php${php_package}-fpm"],
+	}
 }
