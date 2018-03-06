3d7ca4cb280c53805342365fc7af540ee43c7f9a
Bronson Quick <bronson@bronsonquick.com.au>
2018-03-06 13:15:02 +1000

    Clean up linting

--- modules/mailhog/manifests/php.pp
+++ modules/mailhog/manifests/php.pp
@@ -1,20 +1,20 @@
 # A Chassis extension to install and configure MailHog on your server
 class mailhog::php (
-  $php = $::mailhog_config[php]
+	$php = $::mailhog_config[php]
 )
-{
-  if versioncmp( "${$php}", '5.4') <= 0 {
-    $php_package = 'php5'
-    $php_dir = 'php5'
-  }
-  else {
-    $short_ver = regsubst("${php}", '^(\d+\.\d+)\.\d+$', '\1')
-    $php_package = "${short_ver}"
-    $php_dir = "php/${short_ver}"
-  }
-  file { "/etc/${php_dir}/fpm/conf.d/mailhog.ini":
-    content => template('mailhog/php.ini.erb'),
-    notify  => Service["php${php_package}-fpm"],
-    require => Package["php${php_package}-fpm"],
-  }
-}
+	{
+		if versioncmp($php, '5.4') <= 0 {
+			$php_package = 'php5'
+			$php_dir = 'php5'
+		}
+		else {
+			$short_ver = regsubst($php, '^(\d+\.\d+)\.\d+$', '\1')
+			$php_package = $short_ver
+			$php_dir = "php/${short_ver}"
+		}
+		file { "/etc/${php_dir}/fpm/conf.d/mailhog.ini":
+			content => template('mailhog/php.ini.erb'),
+			notify  => Service["php${php_package}-fpm"],
+			require => Package["php${php_package}-fpm"],
+		}
+	}
